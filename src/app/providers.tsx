229fa3d2ff6a78d4b40d4ec47b6c73c93165045f
Abbytec <gpsmurfs@gmail.com>
2025-01-29 20:08:14 -0300

    feature: gatito

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,8 +2,22 @@
 "use client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "../theme/theme";
+import { Neko } from "neko-ts";
+import { useEffect, useRef } from "react";
 
 export default function Providers({ children }: Readonly<{ children: React.ReactNode }>) {
+	const neko = useRef<Neko | null>(null);
+
+	useEffect(() => {
+		if (!neko?.current) {
+			neko.current = new Neko({
+				origin: {
+					x: 100,
+					y: 100,
+				},
+			});
+		}
+	}, [neko]);
 	return (
 		<div suppressHydrationWarning={true}>
 			<ChakraProvider theme={theme}>
