6f529830c7ee498d4f7546a726e6fe4277c5e951
Abbytec <gpsmurfs@gmail.com>
2025-04-15 23:15:47 -0300

    feature: hover effect

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+	return (
+		<footer style={{ textAlign: "center", padding: "1rem" }}>
+			<p>Hecho con amor 💜 - {new Date().getFullYear()}</p>
+		</footer>
+	);
+}
